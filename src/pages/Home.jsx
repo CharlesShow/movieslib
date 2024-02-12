7f07e35b8dfba6e037ccc91e10ec2891f78e9e78
CharlesShow <charlesciqueiramartins@gmail.com>
2024-02-12 19:48:30 -0300

    MoviesLib

---
+++ src/pages/Home.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+import "../css/MoviesGrid.css";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+function Home() {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
+
+    getTopRatedMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length > 0 ? (
+          topMovies.map((movie) => {
+            return <MovieCard key={movie.id} movie={movie} />;
+          })
+        ) : (
+          <p>Carregando...</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/pages/Movie.jsx
@@ -0,0 +1,69 @@
+import { useParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+import { useEffect, useState } from "react";
+import {
+  BsGraphUp,
+  BsWallet2,
+  BsHourglassSplit,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
+import "../css/Movie.css";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+function Movie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setMovie(data);
+  };
+
+  useEffect(() => {
+    const movieUrl = `${moviesUrl}${id}?${apiKey}`;
+    getMovie(movieUrl);
+  }, []);
+
+  function formatCurrency(number) {
+    return number.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  }
+
+  return (
+    <div className="movie-page">
+      {movie && (
+        <>
+          <MovieCard movie={movie} showLink={false} />
+          <p className="tagline">{movie.tagline}</p>
+          <div className="info">
+            <h3>
+              <BsWallet2 /> Orçamento
+            </h3>
+            <p>{formatCurrency(movie.budget)}</p>
+            <h3>
+              <BsGraphUp /> Faturamento:
+            </h3>
+            <p>{formatCurrency(movie.revenue)}</p>
+            <h3>
+              <BsHourglassSplit /> Duração:
+            </h3>
+            <p>{movie.runtime}min</p>
+            <h3>
+              <BsFillFileEarmarkTextFill /> Descrição:
+            </h3>
+            <p>{movie.overview}</p>
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Movie;
